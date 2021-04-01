f2f35bad1145b52db47da268084fdece3b88e392
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-01 14:39:23 +0200

    feat: added shared memory

--- src/building.py
+++ src/building.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from multiprocessing import shared_memory
 
 class Building(object):
-    
-    TupleSpace = {}
-    
+        
     def __init__(self, buildingName="null_name"):
         self.buildingName = buildingName
-        
-        
-        
+        self.tupleSpace = shared_memory.ShareableList()
+                
     def processFireAlarmEvent(self):
         print("Fire alarm event received in building "+ self.buildingName)
+        
+    
+        
+    
         
     
