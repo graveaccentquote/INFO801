95de5e276b02a0098295428f06c32445068f897b
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-01 14:39:02 +0200

    feat: added ControlRoom class

---
+++ src/ControlRoom.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from multiprocessing import shared_memory
+
+from building import Building
+
+class ControlRoom(object):
+    
+    def __init__(self):
+        self.buildingMap = {}
+        
+    def addBuilding(self, building):
+        self.buildingMap[building.name] = building
+     
+        
+    # takes a building name as parameter, polls the list of personel currently
+    # in that building by reading the shared_memory (tuple space) attached to
+    # said building.
+    def pollCurrentlyInPersonel(self, buildingName):
+        self.buildingMap[buildingName].tupleSpace.shm.name
+        
+        
+
