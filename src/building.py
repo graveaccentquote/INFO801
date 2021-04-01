e3277a511eaab4ed3f9d4dc2c9e8718869eae85a
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-01 14:06:03 +0200

    feat: added building class

---
+++ src/building.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class Building(object):
+    
+    TupleSpace = {}
+    
+    def __init__(self, buildingName="null_name"):
+        self.buildingName = buildingName
+        
+        
+        
+    def processFireAlarmEvent():
+        print("Fire alarm event received in building "+self.buildingName)
+        
+    
