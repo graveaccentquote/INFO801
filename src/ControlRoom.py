84d381c40969e8b9c5eb89668e852a92f7d45929
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-09 11:24:15 +0200

    fix: adding/removing priviledges

--- src/ControlRoom.py
+++ src/ControlRoom.py
@@ -1,22 +1,14 @@
 # -*- coding: utf-8 -*-
-
 import memcache
 
-
 def initPersonelList():
-    personelList = []
-    personelList.append({
-        'name' : 'Name1',
-        'id' : 1})
-    personelList.append({
-        'name' : 'Name2',
-        'id' : 2})
-    personelList.append({
-        'name' : 'Name3',
-        'id' : 3})
-    personelList.append({
-        'name' : 'Name4',
-        'id' : 4})
+    personelList = {
+        '1' : {'name' : 'Jean', 'id' : '1'},
+        '2' : {'name' : 'Pierre', 'id' : '2'},
+        '3' : {'name' : 'Marc', 'id' : '3'},
+        '4' : {'name' : 'Paul', 'id' : '4'}
+    }   
+    return personelList
     
 
 class ControlRoom(object):
@@ -24,15 +16,18 @@ class ControlRoom(object):
     def __init__(self, debug=False):
         self.buildingMap = {}
         self.debug = debug
-        self.personelList = []
+        self.personelList = {}
         
         
-    def addBuilding(self, buildingName="default_name", address='127.0.0.1:11211'):
+    def addBuilding(self,
+                    buildingName="default_name",
+                    address='127.0.0.1:11211'):
         sharedMemory = memcache.Client([address], debug=self.debug)
         self.buildingMap[buildingName] = {
             'name' : buildingName, 
             'address' : address, 
-            'memory' : sharedMemory
+            'memory' : sharedMemory,
+            'personel' : []
             }
      
     # takes a building name as parameter, polls the list of personel currently
@@ -40,8 +35,10 @@ class ControlRoom(object):
     # said building.
     def pollCurrentlyInPersonel(self, buildingName):
         sharedMemory = self.buildingMap[buildingName]['memory']
-        #for element in sharedMemory:
-        #    print(element)
+        
+        for personelId in self.buildingMap[buildingName]['personel']:
+            read = sharedMemory.get(personelId)
+            print(read)
     
     def addAccess(self, personelId, buildingName):
         sharedMemory = self.buildingMap[buildingName]['memory']
@@ -53,17 +50,28 @@ class ControlRoom(object):
             'name' : personel['name'],
             'status' : 'AUTHORIZED'
             })
+        
+        self.buildingMap[buildingName]['personel'].append(personel['id'])
     
     def removeAccess(self, personelId, buildingName):
-        #del self.currentPersonel[personelId]
-        print()
+        building = self.buildingMap[buildingName]
+        
+        sharedMemory = building['memory']
+        
+        personel = self.personelList[personelId]
+        
+        sharedMemory.set(personel['id'], None)
+        
+        building['personel'].remove(personel['id'])
         
         
 if __name__ == '__main__':
     controlRoom = ControlRoom(debug=True)
     controlRoom.personelList = initPersonelList()
     controlRoom.addBuilding("8C - Chartreuse")
+    controlRoom.addAccess('1', "8C - Chartreuse")
+    controlRoom.addAccess('2', "8C - Chartreuse")
+    controlRoom.pollCurrentlyInPersonel("8C - Chartreuse")
     
-    
-    #controlRoom.addAccess(1, "8C - Chartreuse")
+    controlRoom.removeAccess('1', "8C - Chartreuse")
     controlRoom.pollCurrentlyInPersonel("8C - Chartreuse")
