df286fd1ae0136b6d5c8c6cac0998f94d788b8e9
bpassieux <bertrand.passieux@gmail.com>
2021-04-28 17:30:14 +0200

    fireAlarm process + remplace Subject par Observable

--- src/fireAlarm.py
+++ src/fireAlarm.py
@@ -2,28 +2,16 @@
 from rx import Observable
 from rx import operators as ops
 from rx import create
-from rx.subject import Subject
-from rx.subject import AsyncSubject
 import keyboard  # using module keyboard
 from multiprocessing import Process
 
-class FireAlarm(object):
+def triggerFireAlarm(subject):
+    subject.subscribe(
+        on_next = lambda i: print("Got - {0}".format(i)),
+        on_error = lambda e: print("Error : {0}".format(e)),
+        on_completed = lambda: print("Job Done!"),
+    )
 
-    def __init__(self, subject):
-        self.subject_keyboard = Subject()
-        self.alarm = subject
-        self.subject_keyboard.subscribe(
-            lambda x: print("Alarm ready {0}".format(x))
-        )
-        self.subject_keyboard.subscribe(
-            lambda y: self.alarm.on_next("A")
-        )
-        
-    def keyboardEvent(self):
-        while True:  # making a loop
-            try:  # used try so that if user pressed other than the given key error will not be shown
-                if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-                    self.subject_keyboard.on_next("A")
-                    break  # finishing the loop
-            except:
-                break  # if user pressed a key other than the given key the loop will break
+def fireAlarm(subject):
+    keyboard.add_hotkey('q', triggerFireAlarm, args=(subject, ))
+    keyboard.wait('esc')
--- src/testAlarm.py
+++ src/testAlarm.py
@@ -1,17 +1,17 @@
-from fireAlarm import FireAlarm
+from fireAlarm import fireAlarm
 from rx import Observable
 from rx import operators as ops
 from rx import create
-from rx.subject import Subject
-from rx.subject import AsyncSubject
 import keyboard  # using module keyboard
 from multiprocessing import Process
+from threading import Thread
 
+
+def alarm_observable(observer, scheduler):
+   observer.on_next("Alarm")
+   observer.on_completed()
 
 if __name__ == '__main__':
-    alarm = Subject()
-    ui = FireAlarm(alarm)
-    alarm.subscribe(
-        lambda x: print("Alarm {0}".format(x))
-    )
-    ui.keyboardEvent()
+    alarm = create(alarm_observable)
+    fAlarm = Thread(target=fireAlarm, args=(alarm,))
+    fAlarm.start()
